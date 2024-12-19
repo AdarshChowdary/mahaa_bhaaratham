dd5bce0ddaec0b1a6134902403d152f8eba097cd
Adarh16 <aaadarsh16@gmail.com>
2024-12-19 17:27:50 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import Hero from './components/Hero.jsx'
 import Mahaabhaaratham from './components/Mahaabhaaratham.jsx'
 import TodayStory from './components/TodayStory.jsx'
 
@@ -11,10 +10,13 @@ const route = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children:[
+      {
+        path: '/mahaabhaaratham', element:<Mahaabhaaratham/>,
+      },
+      { path: '/todaystory', element: <TodayStory />, }
+    ]
   },
-  { path: '/', element: <Hero/>, },
-  { path: '/mahaabhaaratham', element: <Mahaabhaaratham/>, },
-  { path: '/todaystory', element: <TodayStory />, }
 ]);
 
 createRoot(document.getElementById('root')).render(
